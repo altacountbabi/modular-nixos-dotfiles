c02ee8dcfd6b4fb6d2191f59e12fcccff9833c86
altacountbabi <altacountbabi@users.noreply.github.com>
2024-10-23 12:21:33 +0300

    Host specific settings.

--- hosts/laptop-nixos/config.nix
+++ hosts/laptop-nixos/config.nix
@@ -8,6 +8,10 @@
 {
   imports = [ ./hardware.nix ];
 
+  modules = {
+    graphics.gpuType = "nvidia";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
