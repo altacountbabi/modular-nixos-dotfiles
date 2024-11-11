d070e07efd333f966d1c3b88b3f9408443ae9872
altacountbabi <altacountbabi@users.noreply.github.com>
2024-11-11 17:18:36 +0200

    slightly simplify hyprland config

--- modules/optional/desktop/desktops/hyprland.nix
+++ modules/optional/desktop/desktops/hyprland.nix
@@ -58,180 +58,184 @@ mkModule {
     hyprcursor = mkEnableOption "hyprcursor";
   };
   cfg = cfg: { programs.hyprland.enable = true; };
-  hm = cfg: {
-    home.packages = with pkgs; [
-      playerctl # media playback control
-      grimblast # screenshot utility
-      swww # wallpaper daemon
-    ];
+  hm =
+    cfg:
+    let
+      inherit (builtins) concatLists genList toString;
+    in
+    {
+      home.packages = with pkgs; [
+        playerctl # media playback control
+        grimblast # screenshot utility
+        swww # wallpaper daemon
+      ];
 
-    wayland.windowManager.hyprland = {
-      enable = true;
-      catppuccin.enable = config.modules.colorscheme.catppuccin.enable;
-      settings = {
-        "$mod" = "SUPER";
-        bind =
-          [
-            # General Management
-            "$mod SHIFT, M, exit"
-            "$mod, Q, killactive"
+      wayland.windowManager.hyprland = {
+        enable = true;
+        catppuccin.enable = config.modules.colorscheme.catppuccin.enable;
+        settings = {
+          "$mod" = "SUPER";
+          bind =
+            [
+              "$mod SHIFT, M, exit"
+              "$mod, Q, killactive"
 
-            # Layout Management
-            "$mod, V, togglefloating"
-            "$mod, F, fullscreen"
-            "$mod SHIFT, F, fullscreenstate, 0 2"
-            "$mod, P, pseudo"
-            "$mod, J, togglesplit"
-            "$mod, S, togglespecialworkspace, magic"
-            "$mod SHIFT, S, movetoworkspace, special:magic"
+              # Layout Management
+              "$mod, V, togglefloating"
+              "$mod, F, fullscreen"
+              "$mod SHIFT, F, fullscreenstate, 0 2"
+              "$mod, P, pseudo"
+              "$mod, J, togglesplit"
+              "$mod, S, togglespecialworkspace, magic"
+              "$mod SHIFT, S, movetoworkspace, special:magic"
 
-            "$mod SHIFT, Left, swapwindow, l"
-            "$mod SHIFT, Right, swapwindow, r"
-            "$mod SHIFT, Up, swapwindow, u"
-            "$mod SHIFT, Down, swapwindow, d"
+              "$mod SHIFT, Left, swapwindow, l"
+              "$mod SHIFT, Right, swapwindow, r"
+              "$mod SHIFT, Up, swapwindow, u"
+              "$mod SHIFT, Down, swapwindow, d"
 
-            # Window Navigation
-            "$mod, Left, movefocus, l"
-            "$mod, Right, movefocus, r"
-            "$mod, Up, movefocus, u"
-            "$mod, Down, movefocus, d"
+              # Window Navigation
+              "$mod, Left, movefocus, l"
+              "$mod, Right, movefocus, r"
+              "$mod, Up, movefocus, u"
+              "$mod, Down, movefocus, d"
 
-            # Misc
-            "ALT, Tab, exec, ${notifyInfoScript}"
-            "$mod, L, exec, youtube-music"
-            "$mod, T, exec, steam"
+              # Misc
+              "ALT, Tab, exec, ${notifyInfoScript}"
+              "$mod, L, exec, youtube-music"
+              "$mod, T, exec, steam"
 
-            # Screenshotting
-            "ALT, R, exec, grimblast --freeze copy area"
-            "ALT SHIFT, R, exec, grimblast copy screen"
+              # Screenshotting
+              "ALT, R, exec, grimblast --freeze copy area"
+              "ALT SHIFT, R, exec, grimblast copy screen"
 
-            # Media Control
-            # (this makes a lot more sense on my keyboard instead of XF86 keys)
-            "ALT, 7, exec, playerctl previous"
-            "ALT, 8, exec, playerctl play-pause"
-            "ALT, 9, exec, playerctl next"
-          ]
-          # Workspace Switching
-          ++ (builtins.concatLists (
-            builtins.genList (
-              x:
-              let
-                ws =
-                  let
-                    c = (x + 1) / 10;
-                  in
-                  builtins.toString (x + 1 - (c * 10));
-              in
-              [
-                "$mod, ${ws}, workspace, ${toString (x + 1)}"
-                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-              ]
-            ) 10
-          ));
-        binde = [
-          # Volume Keys
-          "ALT, 0, exec, ${volumeScript} t"
-          "ALT, minus, exec, ${volumeScript} d ${toString cfg.volumeStep}"
-          "ALT, equal, exec, ${volumeScript} i ${toString cfg.volumeStep}"
+              # Media Control
+              # (this makes a lot more sense on my keyboard instead of XF86 keys)
+              "ALT, 7, exec, playerctl previous"
+              "ALT, 8, exec, playerctl play-pause"
+              "ALT, 9, exec, playerctl next"
+            ]
+            # Workspace Switching
+            ++ (concatLists (
+              genList (
+                x:
+                let
+                  ws =
+                    let
+                      c = (x + 1) / 10;
+                    in
+                    toString (x + 1 - (c * 10));
+                in
+                [
+                  "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                  "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+                ]
+              ) 10
+            ));
+          binde = [
+            # Volume Keys
+            "ALT, 0, exec, ${volumeScript} t"
+            "ALT, minus, exec, ${volumeScript} d ${toString cfg.volumeStep}"
+            "ALT, equal, exec, ${volumeScript} i ${toString cfg.volumeStep}"
 
-          # Window Resizing
-          "$mod CTRL, Right, resizeactive, ${toString cfg.kbResizeStep} 0"
-          "$mod CTRL, Left, resizeactive, -${toString cfg.kbResizeStep} 0"
-          "$mod CTRL, Up, resizeactive, 0 -${toString cfg.kbResizeStep}"
-          "$mod CTRL, Down, resizeactive, 0 ${toString cfg.kbResizeStep}"
-        ];
-        bindm = [
-          "$mod, mouse:272, movewindow"
-          "$mod, mouse:273, resizewindow"
-        ];
-        exec-once = [
-          "swww-daemon"
-          "${wallpaperScript}"
-        ];
-
-        input = with config.modules.graphics; {
-          # Keyboard
-          kb_layout = xkb.layout;
-          kb_variant = xkb.variant;
-          kb_options = xkb.options;
-          numlock_by_default = false;
-
-          # Mouse
-          sensitivity = -0.1;
-          follow_mouse = 1;
-          mouse_refocus = false;
-          accel_profile = "flat";
-
-          # Touchpad
-          touchpad = {
-            natural_scroll = true;
-            drag_lock = true;
-          };
-        };
-
-        device = {
-          # Enable mouse acceleration for touchpad.
-          name = cfg.touchpadName;
-          accel_profile = "adaptive";
-        };
-
-        general = {
-          gaps_in = 5;
-          gaps_out = 10;
-          border_size = 0;
-          layout = "dwindle";
-        };
-
-        decoration = {
-          rounding = 15;
-
-          blur = {
-            enabled = true;
-            size = 10;
-            passes = 4;
-            noise = 0.1;
-          };
-
-          drop_shadow = true;
-          shadow_range = 14;
-          shadow_render_power = 3;
-          shadow_ignore_window = true;
-          "col.shadow" = "rgba(00000045)";
-        };
-
-        animations = {
-          enabled = true;
-
-          bezier = "smallOvershoot, 0.05, 0.9, 0, 1.05";
-          animation = [
-            "windows, 1, 7, smallOvershoot, slide"
-            "windowsOut, 1, 7, default, slide"
-            "border, 1, 10, default"
-            "borderangle, 1, 8, default"
-            "fade, 1, 7, default"
-            "workspaces, 1, 6, smallOvershoot"
+            # Window Resizing
+            "$mod CTRL, Right, resizeactive, ${toString cfg.kbResizeStep} 0"
+            "$mod CTRL, Left, resizeactive, -${toString cfg.kbResizeStep} 0"
+            "$mod CTRL, Up, resizeactive, 0 -${toString cfg.kbResizeStep}"
+            "$mod CTRL, Down, resizeactive, 0 ${toString cfg.kbResizeStep}"
           ];
+          bindm = [
+            "$mod, mouse:272, movewindow"
+            "$mod, mouse:273, resizewindow"
+          ];
+          exec-once = [
+            "swww-daemon"
+            "${wallpaperScript}"
+          ];
+
+          input = with config.modules.graphics; {
+            # Keyboard
+            kb_layout = xkb.layout;
+            kb_variant = xkb.variant;
+            kb_options = xkb.options;
+            numlock_by_default = false;
+
+            # Mouse
+            sensitivity = -0.1;
+            follow_mouse = 1;
+            mouse_refocus = false;
+            accel_profile = "flat";
+
+            # Touchpad
+            touchpad = {
+              natural_scroll = true;
+              drag_lock = true;
+            };
+          };
+
+          device = {
+            # Enable mouse acceleration for touchpad.
+            name = cfg.touchpadName;
+            accel_profile = "adaptive";
+          };
+
+          general = {
+            gaps_in = 5;
+            gaps_out = 10;
+            border_size = 0;
+            layout = "dwindle";
+          };
+
+          decoration = {
+            rounding = 15;
+
+            blur = {
+              enabled = true;
+              size = 10;
+              passes = 4;
+              noise = 0.1;
+            };
+
+            drop_shadow = true;
+            shadow_range = 14;
+            shadow_render_power = 3;
+            shadow_ignore_window = true;
+            "col.shadow" = "rgba(00000045)";
+          };
+
+          animations = {
+            enabled = true;
+
+            bezier = "smallOvershoot, 0.05, 0.9, 0, 1.05";
+            animation = [
+              "windows, 1, 7, smallOvershoot, slide"
+              "windowsOut, 1, 7, default, slide"
+              "border, 1, 10, default"
+              "borderangle, 1, 8, default"
+              "fade, 1, 7, default"
+              "workspaces, 1, 6, smallOvershoot"
+            ];
+          };
+
+          dwindle = {
+            pseudotile = true;
+            preserve_split = true;
+            no_gaps_when_only = true;
+          };
+
+          cursor.enable_hyprcursor = cfg.hyprcursor;
+
+          misc = {
+            disable_hyprland_logo = true;
+            disable_splash_rendering = true;
+            animate_manual_resizes = true;
+            enable_swallow = true;
+            swallow_exception_regex = "(qemu|wev)";
+            initial_workspace_tracking = 0;
+          };
+
+          xwayland.force_zero_scaling = true;
         };
-
-        dwindle = {
-          pseudotile = true;
-          preserve_split = true;
-          no_gaps_when_only = true;
-        };
-
-        cursor.enable_hyprcursor = cfg.hyprcursor;
-
-        misc = {
-          disable_hyprland_logo = true;
-          disable_splash_rendering = true;
-          animate_manual_resizes = true;
-          enable_swallow = true;
-          swallow_exception_regex = "(qemu|wev)";
-          initial_workspace_tracking = 0;
-        };
-
-        xwayland.force_zero_scaling = true;
       };
     };
-  };
 }
