595d247c8eecdfba4e1f930df1986711c52e49b6
altacountbabi <altacountbabi@users.noreply.github.com>
2024-12-05 23:27:45 +0200

    Generate hardware config for drivers and whatnot.

--- pkgs/installer/main.sh
+++ pkgs/installer/main.sh
@@ -21,6 +21,7 @@ export GUM_INPUT_CURSOR_FOREGROUND=""
 export GUM_CONFIRM_PROMPT_FOREGROUND=""
 export GUM_CONFIRM_SELECTED_BACKGROUND="4"
 export GUM_SPIN_SPINNER_FOREGROUND="4"
+export GUM_SPIN_SPINNER="line"
 
 HOSTS=$(find /tmp/dotfiles/hosts -type f -name "config.nix" -printf "%h\n" | awk -F'/' '{print $(NF)}')
 CHOICES=$(echo -e "$HOSTS\nCreate New")
@@ -44,7 +45,11 @@ if [ "$TARGET_HOST" = "Create New" ]; then
     TARGET_HOST=$NEW_HOST_NAME
 fi
 
-# Prompt for what disk to install to
+gum spin --title "Generating hardware config..." -- nixos-generate-config --root /tmp/hwconfig --no-filesystems
+cp "/tmp/hwconf/etc/nixos/hardware-configuration.nix" /tmp/dotfiles/hosts/"$NEW_HOST_NAME"/hardware.nix
+
+# Prompt for what disk to install to:
+# list disks | remove first line of lsblk | prefix disk name with `/dev/` | choose from disks
 DISK=$(lsblk -d -o NAME,TYPE | grep -E 'disk|part' | awk '{print "/dev/" $1}' | gum choose --header "What disk do you want to install to?")
 
 # Set disko module `device` option
@@ -55,5 +60,5 @@ if ! gum confirm --default=false "Are you sure you want to install to $DISK? Thi
     exit 0
 fi
 
-gum spin --spinner line --title "Partitioning disks..." -- sudo nix run github:nix-community/disko --extra-experimental-features "nix-command flakes" --no-write-lock-file -- --mode destroy,format,mount "/tmp/dotfiles/pkgs/installer/disko.nix"
-gum spin --spinner line --title "Installing NixOS... (this may take a while)" -- sudo nixos-install --flake "/tmp/dotfiles/#$TARGET_HOST"
+gum spin --title "Partitioning disks..." -- sudo nix run github:nix-community/disko --extra-experimental-features "nix-command flakes" --no-write-lock-file -- --mode destroy,format,mount "/tmp/dotfiles/pkgs/installer/disko.nix"
+gum spin --title "Installing NixOS... (this may take a while)" -- sudo nixos-install --flake "/tmp/dotfiles/#$TARGET_HOST"
