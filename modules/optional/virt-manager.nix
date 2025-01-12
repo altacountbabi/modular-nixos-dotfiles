d1005fcef82aebbdd4542265f9e5ac2f2ef6a2ac
altacountbabi <altacountbabi@users.noreply.github.com>
2025-01-12 16:06:42 +0200

    Fix virt-manager module

--- modules/optional/virt-manager.nix
+++ modules/optional/virt-manager.nix
@@ -11,11 +11,11 @@ mkModule {
   cfg = cfg: {
     programs.virt-manager.enable = true;
     virtualisation.libvirtd.enable = true;
-
+    users.groups.libvirtd.members = [ config.modules.user.username ];
+  };
+  hm = cfg: {
     home.packages = with pkgs; [
       qemu
     ];
-
-    users.groups.libvirtd.members = [ config.modules.user.username ];
   };
 }
