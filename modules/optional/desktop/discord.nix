30b6d66cbeb370a9792b38e221a9b9f570c1b8bc
altacountbabi <altacountbabi@users.noreply.github.com>
2025-01-12 18:30:51 +0200

    Fix discord quick css

--- modules/optional/desktop/discord.nix
+++ modules/optional/desktop/discord.nix
@@ -1,7 +1,15 @@
-{ mkModule, config, inputs, lib, ... }:
+{
+  mkModule,
+  config,
+  inputs,
+  lib,
+  ...
+}:
 
-let inherit (lib) mkEnableOption mkIf;
-in mkModule {
+let
+  inherit (lib) mkEnableOption mkIf;
+in
+mkModule {
   name = "desktop.discord";
   path = "desktop.discord";
   opts.autoStart = mkEnableOption "Discord as a startup app";
@@ -10,19 +18,23 @@ in mkModule {
 
     programs.nixcord = {
       enable = true;
-      quickCss = (if config.modules.colorscheme.catppuccin.enable then
-        ''
-          import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css");''
-      else
-        "") + builtins.readFile (builtins.fetchurl {
-          url =
-            "https://gist.githubusercontent.com/altacountbabi/fbb93e52946c2571b678119e0f4f8b69/raw/88c3951022b2763bf4f7680494ceaf4008503c40/style.css";
-          sha256 =
-            "5acf36750b807d6a78c4b66b1259ec8a7243e64dfce8ca2c8982676c76c8ba6c";
-        });
+      quickCss =
+        (
+          if config.modules.colorscheme.catppuccin.enable then
+            ''
+              @import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css");
+            ''
+          else
+            ""
+        )
+        + builtins.readFile (
+          builtins.fetchurl {
+            url = "https://gist.githubusercontent.com/altacountbabi/fbb93e52946c2571b678119e0f4f8b69/raw/88c3951022b2763bf4f7680494ceaf4008503c40/style.css";
+            sha256 = "5acf36750b807d6a78c4b66b1259ec8a7243e64dfce8ca2c8982676c76c8ba6c";
+          }
+        );
 
-      discord.openASAR.enable =
-        false; # OpenASAR seems to be missing some kind of module, maybe I'll figure it out later.
+      discord.openASAR.enable = false; # OpenASAR seems to be missing some kind of module, maybe I'll figure it out later.
       config = {
         useQuickCss = true;
         frameless = true;
@@ -60,14 +72,15 @@ in mkModule {
     };
 
     wayland.windowManager.hyprland.settings =
-      mkIf (config.modules.desktop.desktops.hyprland.enable && cfg.autoStart) {
-        exec-once = [
-          "[workspace 2 silent] discord --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland"
-        ];
-        windowrulev2 = [
-          # [workspace 2 silent] in exec-once only seems to apply to the launcher not the whole app
-          "workspace 2 silent,class:(discord)"
-        ];
-      };
+      mkIf (config.modules.desktop.desktops.hyprland.enable && cfg.autoStart)
+        {
+          exec-once = [
+            "[workspace 2 silent] discord --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland"
+          ];
+          windowrulev2 = [
+            # [workspace 2 silent] in exec-once only seems to apply to the launcher not the whole app
+            "workspace 2 silent,class:(discord)"
+          ];
+        };
   };
 }
