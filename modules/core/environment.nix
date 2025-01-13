aaf0fb265b53cadc341414010edfa3f6efec80bb
altacountbabi <altacountbabi@users.noreply.github.com>
2025-01-13 13:17:06 +0200

    Add env variable to fix audio crackling

--- modules/core/environment.nix
+++ modules/core/environment.nix
@@ -14,6 +14,8 @@ mkModule {
       EDITOR = cfg.editor;
       MICRO_TRUECOLOR = 1;
       NIXOS_OZONE_WL = "1";
+      # Fix audio crackling/popping in some games
+      PULSE_LATENCY_MSEC = 50;
     };
   };
 }
