512debca374d32bca943cc0f526667163ab26c7f
altacountbabi <altacountbabi@users.noreply.github.com>
2025-02-12 22:23:47 +0200

    Add libstdc++.so to $LD_LIBRARY_PATH

--- modules/core/environment.nix
+++ modules/core/environment.nix
@@ -1,4 +1,9 @@
-{ mkModule, config, ... }:
+{
+  mkModule,
+  config,
+  pkgs,
+  ...
+}:
 
 mkModule {
   name = "environment variables";
@@ -9,6 +14,8 @@ mkModule {
       ${if config.modules.graphics.gpuType != "nvidia" then "NIXOS_OZONE_WL" else null} = "1";
       # Fix audio crackling/popping in some games
       PULSE_LATENCY_MSEC = 50;
+      # Fix issues with tree-sitter using libstdc++ at runtime
+      LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
     };
   };
 }
