4230b03beaca4d929da7fc8b971af78764ae5d28
altacountbabi <altacountbabi@users.noreply.github.com>
2025-02-28 21:52:47 +0200

    webkitgtk is a piece of shit

--- modules/core/user/shells/nushell/default.nix
+++ modules/core/user/shells/nushell/default.nix
@@ -21,7 +21,12 @@ mkModule {
   hm = cfg: {
     programs.nushell = {
       enable = true;
-      configFile.source = ./config.nu;
+      configFile.text =
+        (builtins.readFile ./config.nu)
+        + (with pkgs; ''
+          $env.XDG_DATA_DIRS = $"${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:($env.XDG_DATA_DIRS)";
+          $env.GIO_MODULE_DIR = "${glib-networking}/lib/gio/modules/";
+        '');
       envFile.source = ./env.nu;
     };
   };
