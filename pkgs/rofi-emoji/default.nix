a9c4dab528ddf772d93b3a3e5cea0a166f5834d8
altacountbabi <altacountbabi@users.noreply.github.com>
2025-05-15 19:19:17 +0300

    formatting

--- pkgs/rofi-emoji/default.nix
+++ pkgs/rofi-emoji/default.nix
@@ -5,7 +5,5 @@
 }:
 
 (if wayland then pkgs.rofi-emoji else pkgs.rofi-emoji-wayland).overrideAttrs (prev: {
-  patches = prev.patches ++ [
-    ./emojis.patch
-  ];
+  patches = prev.patches ++ [ ./emojis.patch ];
 })
